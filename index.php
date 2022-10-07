0cec664e517ba7f3d3ff6f3ebaefdc952382b103
Aleks38 <alexy.dasilva@icloud.com>
2022-10-07 17:45:02 +0200

    Page présentation, bouton CV, projet scolaire

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
-<link rel="stylesheet" href="style.css">
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    
+        <div class="header-full"> 
+            <div class="center-div">
+                <div>
+                    test
+                </div>
+            </div>
+        </div>
 
+        <hr class="line">
+        <div class="d-grid col-2 mx-auto my-5">
+            <button type="button" class="btn btn-lg btn-primary rounded-pill theme border-0">Curriculum vitæ</button>
+        </div>
 
-<p>test</p>
+        <div class="container text-center">
+            <div class="row ">
+                <div class="col theme">
+                    1 of 3
+                </div>
+                <div class="col theme">
+                    <hr class="vertical mx-auto rounded-5">
+                </div>
+                <div class="col theme">
+                    3 of 3
+                </div>
+            </div>
 
-<html>
-    <div class="center-div"></div>
+        </div>
 
+    </body>
 </html>
 
 
