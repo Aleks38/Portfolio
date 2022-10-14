e6fed78dd3b4896945e0af317ae489a5f568b32d
Aleks38 <alexy.dasilva@icloud.com>
2022-10-14 15:39:51 +0200

    ajout

--- index.php
+++ index.php
@@ -25,19 +25,49 @@
 
         <div class="container text-center">
             <div class="row ">
-                <div class="col theme">
-                    1 of 3
+                <div class="col theme row mb-0">
+                    <div class="mb-0">
+                        <div class="test my-2 fw-bold lighThemeText">
+                            <p class="text-start">2021 - Ajourd'hui</p>
+                        </div>
+                        <div class="test offset-md-1 my-2 h-25">
+                            <p class="text-start">BTS SIO option SLAM** à l’institut supérieur Saint-Denis 07100 Annonay </p>
+                        </div>
+                        <div class="test my-2 fw-bold lighThemeText">
+                            <p class="text-start">2019 - 2021</p>
+                        </div>
+                        <div class="test offset-md-1 my-2 h-25">
+                            <p class="text-start">Bac Général option Mathématques, Physique Chimie et Numérique et Science Informatique</p>
+                        </div>  
+                    </div>
                 </div>
-                <div class="col theme">
-                    <hr class="vertical mx-auto rounded-5">
+                <div class="col-1 theme mb-0">
+                    <hr class="vertical mx-auto rounded-5 lighThemeBack">
                 </div>
-                <div class="col theme">
-                    3 of 3
+                <div class="col theme row mb-0">
+                    <div class="mb-0">
+                        <div class="test my-2 fw-bold lighThemeText">
+                            <p class="text-start">2023 - 2025</p>
+                        </div>
+
+                        <div class="test offset-md-1 my-2 h-25">
+                            <p class="text-start">Master Développement logiciel</p>
+                        </div>
+                        
+                        <div class="test my-2 fw-bold lighThemeText">
+                            <p class="text-start">2022 - 2023</p>
+                        </div>
+                        <div class="test offset-md-1 my-2 h-25">
+                            <p class="text-start">Licence Pro dans le développement informatique</p>
+                        </div>
+                    </div>
                 </div>
+                
+                <p class="text-start fs-6">**Solution logiciel et application métier </p>
             </div>
+            
 
         </div>
-
     </body>
 </html>
 
