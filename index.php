6133608a4b36e4b6f254d4b2c34a04089069c786
Alexy Da Silva <alexy.dasilva@icloud.com>
2022-12-24 03:01:05 +0100

    mise en place de la partie projet

--- index.php
+++ index.php
@@ -203,11 +203,11 @@
                         </a>
 
                         <!-- Mail -->
-                        <a href="#contact" class="text-decoration-none">
+                        <!-- <a href="" class="text-decoration-none" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="Bottom popover" aria-describedby="popover528830">
                             <svg class="iconPresentation mx-2" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-envelope-paper-fill mx-2" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M6.5 9.5 3 7.5v-6A1.5 1.5 0 0 1 4.5 0h7A1.5 1.5 0 0 1 13 1.5v6l-3.5 2L8 8.75l-1.5.75ZM1.059 3.635 2 3.133v3.753L0 5.713V5.4a2 2 0 0 1 1.059-1.765ZM16 5.713l-2 1.173V3.133l.941.502A2 2 0 0 1 16 5.4v.313Zm0 1.16-5.693 3.337L16 13.372v-6.5Zm-8 3.199 7.941 4.412A2 2 0 0 1 14 16H2a2 2 0 0 1-1.941-1.516L8 10.072Zm-8 3.3 5.693-3.162L0 6.873v6.5Z"/>
                             </svg>
-                        </a>
+                        </a> -->
                     </div>
                 </div>
             </div>
@@ -239,7 +239,7 @@
                                 <p class="text-start text-purple">2021 - Ajourd'hui</p>
                             </div>
                             <div class="offset-md-1 my-2 col-md-11">
-                                <p class="text-start">BTS SIO option SLAM* à l’institut supérieur Saint-Denis 07100 Annonay </p>
+                                <p class="text-start">BTS SIO option SLAM* à l'institut supérieur Saint-Denis 07100 Annonay </p>
                             </div>
                             <div class="my-2 fw-bold">
                                 <p class="text-start text-purple">2019 - 2021</p>
@@ -453,7 +453,7 @@
 
             <!-- Presentation BTS SIO -->
             <div class="col-6 mx-auto mb-15" id="BTS">
-                <h1 class="fs-4 mb-5 text-white">Le BTS SIO :</h1>
+                <p class="fs-4 mb-5 text-white">Le BTS SIO :</p>
                 <p class="lh-sm text-white">
                     Le <span class="badge rounded-pill text-bg-purple">BTS SIO</span> (Brevet de Technicien Supérieur en Services Informatiques aux Organisations) est un diplôme de niveau bac+2 qui forme les étudiants aux métiers de l'informatique et du développement d'applications. Il est proposé dans les lycées et les centres de formation en France. 
                     <br><br>
@@ -468,6 +468,152 @@
                 </p>
             </div>
             <!-- Fin presentation BTS SIO -->
+
+            <!-- Projets -->
+            <div class="container" id="projet">
+                <p class="fs-4 mb-5 text-white ms-5">Mes projets :</p>
+                <div class="row col-10 mx-auto mb-15 " >
+                    
+                    <!-- Projet Gestion Immobilière -->
+                    <div class="example m-3 p-0 rounded-3" style="width: 300px; height: 450px;" >
+                        <img class="imgCards" src="images/Projet_Gestion-Immobiliere_miniature.jpg" />
+                        
+                        <div class="fadedbox text-start p-3">
+                            <p class="fs-4 fw-bold mb-3 card-title d-block text-start">Gestion Immobilière</p>
+                            <p class="fs-6 mb-3 card-text d-block">Le but de ce projet est de pouvoir suivre son patrimoine immobilié grâce à une application sur votre bureau et avoir quelque fonctionnalité en plus. Pour cela nous avons utilisé le framwork <strong>WPF</strong> avec l'ORM <strong>Entity Framework</strong>. </p>
+                            <p class="fs-6 fw-bold card-text d-block">BTS SIO 2eme année</p>
+                            
+                            <div class="d-block my-4">
+                                <div class="d-inline">
+                                    <svg height="2em" width="2em" preserveAspectRatio="xMidYMid" viewBox="0 -1.428 255.582 290.108" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="m255.569 84.452c-.002-4.83-1.035-9.098-3.124-12.76-2.052-3.603-5.125-6.622-9.247-9.009-34.025-19.619-68.083-39.178-102.097-58.817-9.17-5.294-18.061-5.1-27.163.27-13.543 7.986-81.348 46.833-101.553 58.536-8.321 4.818-12.37 12.19-12.372 21.771-.013 39.455 0 78.91-.013 118.365 0 4.724.991 8.91 2.988 12.517 2.053 3.711 5.169 6.813 9.386 9.254 20.206 11.703 88.02 50.547 101.56 58.536 9.106 5.373 17.997 5.565 27.17.27 34.015-19.64 68.075-39.199 102.105-58.818 4.217-2.44 7.333-5.544 9.386-9.252 1.994-3.608 2.987-7.793 2.987-12.518 0 0 0-78.889-.013-118.345" fill="#a179dc"/>
+                                        <path d="m128.182 143.241-125.194 72.084c2.053 3.711 5.169 6.813 9.386 9.254 20.206 11.703 88.02 50.547 101.56 58.536 9.106 5.373 17.997 5.565 27.17.27 34.015-19.64 68.075-39.199 102.105-58.818 4.217-2.44 7.333-5.544 9.386-9.252z" fill="#280068"/>
+                                        <path d="m255.569 84.452c-.002-4.83-1.035-9.098-3.124-12.76l-124.263 71.55 124.413 72.073c1.994-3.608 2.985-7.793 2.987-12.518 0 0 0-78.889-.013-118.345" fill="#390091"/>
+                                        <g fill="#fff">
+                                            <path d="m201.892 116.294v13.474h13.474v-13.474h6.737v13.474h13.474v6.737h-13.474v13.473h13.474v6.737h-13.474v13.474h-6.737v-13.474h-13.474v13.474h-6.737v-13.474h-13.473v-6.737h13.473v-13.473h-13.473v-6.737h13.473v-13.474zm13.474 20.21h-13.474v13.474h13.474z"/>
+                                            <path d="m128.457 48.626c35.144 0 65.827 19.086 82.262 47.456l-.16-.273-41.35 23.808c-8.146-13.793-23.08-23.102-40.213-23.294l-.54-.003c-26.125 0-47.305 21.18-47.305 47.305a47.08 47.08 0 0 0 6.239 23.47c8.154 14.235 23.483 23.836 41.067 23.836 17.693 0 33.109-9.723 41.221-24.11l-.197.345 41.287 23.918c-16.255 28.13-46.518 47.157-81.253 47.536l-1.058.006c-35.255 0-66.025-19.204-82.419-47.724-8.003-13.923-12.582-30.064-12.582-47.277 0-52.466 42.532-95 95-95z"/>
+                                        </g>
+                                    </svg>
+                                </div>
+
+                                <p class="m-0 d-inline">
+                                    C# WPF
+                                </p>
+                            </div>
+
+                            <a href="#" class="d-block">
+                                <div>
+                                    <p class="fs-6 rounded-5 d-inline position-absolute px-3 py-1 btnMoreCs">
+                                        Voir plus
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- Fin projet Gestion Immobilière -->
+
+                    <!-- Projet Overwatch Site -->
+                    <div class="example m-3 p-0 rounded-3" style="width: 300px; height: 450px;">
+                        <img class="imgCards" src="images/Projet_Overwatch_miniature.jpg" />
+                        
+                        <div class="fadedbox text-start p-3">
+                            <p class="fs-4 fw-bold mb-3 card-title d-block text-start">Overwatch site</p>
+                            <p class="fs-6 mb-3 card-text d-block">Ce projet était un exercice de fin de première année. Le but était de faire un site sur un sujet que nous aimions tout en ayant une relation avec une <strong>base de donnée</strong> pour qu'un utilisateur puisse se connecter.</p>
+                            <p class="fs-6 fw-bold card-text d-block">BTS SIO 1re année</p>
+                          
+                            <div class="d-block my-4">
+                                <div class="d-inline">
+                                    <svg width="2em" height="1em" viewBox="0 0 256 134" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet">
+                                        <g fill-rule="evenodd">
+                                            <ellipse fill="#8993BE" cx="128" cy="66.63" rx="128" ry="66.63"/>
+                                            <path d="M35.945 106.082l14.028-71.014H82.41c14.027.877 21.041 7.89 21.041 20.165 0 21.041-16.657 33.315-31.562 32.438H56.11l-3.507 18.411H35.945zm23.671-31.561L64 48.219h11.397c6.137 0 10.52 2.63 10.52 7.89-.876 14.905-7.89 17.535-15.78 18.412h-10.52zM100.192 87.671l14.027-71.013h16.658l-3.507 18.41h15.78c14.028.877 19.288 7.89 17.535 16.658l-6.137 35.945h-17.534l6.137-32.438c.876-4.384.876-7.014-5.26-7.014H124.74l-7.89 39.452h-16.658zM153.425 106.082l14.027-71.014h32.438c14.028.877 21.042 7.89 21.042 20.165 0 21.041-16.658 33.315-31.562 32.438h-15.781l-3.507 18.411h-16.657zm23.67-31.561l4.384-26.302h11.398c6.137 0 10.52 2.63 10.52 7.89-.876 14.905-7.89 17.535-15.78 18.412h-10.521z" fill="#232531"/>
+                                        </g>
+                                    </svg>
+                                </div>
+
+                                <p class="m-0 d-inline">
+                                    PHP Natif
+                                </p>
+                            </div>
+
+                            <a href="#" class="d-block">
+                                <div>
+                                    <p class="fs-6 rounded-5 d-inline position-absolute px-3 py-1 btnMorePHP">
+                                        Voir plus
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- Fin projet Overwatch Site -->
+
+                    <!-- Projet Pygame -->
+                    <div class="example m-3 p-0 rounded-3" style="width: 300px; height: 450px;">
+                        <img class="imgCards" src="images/Projet_Pygame_miniature.jpg" />
+                        
+                        <div class="fadedbox text-start p-3">
+                            <p class="fs-4 fw-bold mb-3 card-title d-block text-start">Le jeu le plus dur du monde</p>
+                            <p class="fs-6 mb-3 card-text d-block">Ce projet est un jeu s'inspirant fortement du jeu qui s'appel "World's Hardest Game". Le jeu a été réalisé avec deux autres personnes, nous nous sommes donc répartit les tâches. La technologie utilisée est la bibliothèque python pygame. </p>
+                            <p class="fs-6 fw-bold card-text d-block">BTS SIO 1re année</p>
+                            
+                            <div class="d-block my-4">
+                                <div class="d-inline">
+                                    <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="2em" height="em">
+                                        <path fill="#0277BD" d="M24.047,5c-1.555,0.005-2.633,0.142-3.936,0.367c-3.848,0.67-4.549,2.077-4.549,4.67V14h9v2H15.22h-4.35c-2.636,0-4.943,1.242-5.674,4.219c-0.826,3.417-0.863,5.557,0,9.125C5.851,32.005,7.294,34,9.931,34h3.632v-5.104c0-2.966,2.686-5.896,5.764-5.896h7.236c2.523,0,5-1.862,5-4.377v-8.586c0-2.439-1.759-4.263-4.218-4.672C27.406,5.359,25.589,4.994,24.047,5z M19.063,9c0.821,0,1.5,0.677,1.5,1.502c0,0.833-0.679,1.498-1.5,1.498c-0.837,0-1.5-0.664-1.5-1.498C17.563,9.68,18.226,9,19.063,9z"/><path fill="#FFC107" d="M23.078,43c1.555-0.005,2.633-0.142,3.936-0.367c3.848-0.67,4.549-2.077,4.549-4.67V34h-9v-2h9.343h4.35c2.636,0,4.943-1.242,5.674-4.219c0.826-3.417,0.863-5.557,0-9.125C41.274,15.995,39.831,14,37.194,14h-3.632v5.104c0,2.966-2.686,5.896-5.764,5.896h-7.236c-2.523,0-5,1.862-5,4.377v8.586c0,2.439,1.759,4.263,4.218,4.672C19.719,42.641,21.536,43.006,23.078,43z M28.063,39c-0.821,0-1.5-0.677-1.5-1.502c0-0.833,0.679-1.498,1.5-1.498c0.837,0,1.5,0.664,1.5,1.498C29.563,38.32,28.899,39,28.063,39z"/>
+                                    </svg>
+                                </div>
+
+                                <p class="m-0 d-inline">
+                                    Python Pygame
+                                </p>
+                            </div>
+
+                            <a href="#" class="d-block">
+                                <div>
+                                    <p class="fs-6 rounded-5 d-inline position-absolute px-3 py-1 btnMorePy">
+                                        Voir plus
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- Fin projet Pygame -->
+
+                    <!-- Projet Laravel -->
+                    <div class="example m-3 p-0 rounded-3" style="width: 300px; height: 450px;">
+                        <img class="imgCards" src="images/Projet_Laravel_miniature.jpg" />
+                        
+                        <div class="fadedbox text-start p-3">
+
+                            <p class="fs-4 fw-bold mb-3 card-title d-block text-start">Gestion de ticket</p>
+                            <p class="fs-6 mb-3 card-text d-block">Ce projet est est une application web que j'ai reprise en cours de développement lors de mon stage chez <strong>Fitnesse Boutique</strong> à Voiron. Il consistait à trier les différents tickets qui arrivaient au service informatique de l'entreprise et de pouvoir en avoir une trace et donc les retrouver. </p>
+                            <p class="fs-6 fw-bold card-text d-block">Stage BTS SIO 1re année</p>
+                            
+                            <div class="d-block my-4">
+                                <div class="d-inline">
+                                    <svg  viewBox="0 -.11376601 49.74245785 51.31690859" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="m49.626 11.564a.809.809 0 0 1 .028.209v10.972a.8.8 0 0 1 -.402.694l-9.209 5.302v10.509c0 .286-.152.55-.4.694l-19.223 11.066c-.044.025-.092.041-.14.058-.018.006-.035.017-.054.022a.805.805 0 0 1 -.41 0c-.022-.006-.042-.018-.063-.026-.044-.016-.09-.03-.132-.054l-19.219-11.066a.801.801 0 0 1 -.402-.694v-32.916c0-.072.01-.142.028-.21.006-.023.02-.044.028-.067.015-.042.029-.085.051-.124.015-.026.037-.047.055-.071.023-.032.044-.065.071-.093.023-.023.053-.04.079-.06.029-.024.055-.05.088-.069h.001l9.61-5.533a.802.802 0 0 1 .8 0l9.61 5.533h.002c.032.02.059.045.088.068.026.02.055.038.078.06.028.029.048.062.072.094.017.024.04.045.054.071.023.04.036.082.052.124.008.023.022.044.028.068a.809.809 0 0 1 .028.209v20.559l8.008-4.611v-10.51c0-.07.01-.141.028-.208.007-.024.02-.045.028-.068.016-.042.03-.085.052-.124.015-.026.037-.047.054-.071.024-.032.044-.065.072-.093.023-.023.052-.04.078-.06.03-.024.056-.05.088-.069h.001l9.611-5.533a.801.801 0 0 1 .8 0l9.61 5.533c.034.02.06.045.09.068.025.02.054.038.077.06.028.029.048.062.072.094.018.024.04.045.054.071.023.039.036.082.052.124.009.023.022.044.028.068zm-1.574 10.718v-9.124l-3.363 1.936-4.646 2.675v9.124l8.01-4.611zm-9.61 16.505v-9.13l-4.57 2.61-13.05 7.448v9.216zm-36.84-31.068v31.068l17.618 10.143v-9.214l-9.204-5.209-.003-.002-.004-.002c-.031-.018-.057-.044-.086-.066-.025-.02-.054-.036-.076-.058l-.002-.003c-.026-.025-.044-.056-.066-.084-.02-.027-.044-.05-.06-.078l-.001-.003c-.018-.03-.029-.066-.042-.1-.013-.03-.03-.058-.038-.09v-.001c-.01-.038-.012-.078-.016-.117-.004-.03-.012-.06-.012-.09v-21.483l-4.645-2.676-3.363-1.934zm8.81-5.994-8.007 4.609 8.005 4.609 8.006-4.61-8.006-4.608zm4.164 28.764 4.645-2.674v-20.096l-3.363 1.936-4.646 2.675v20.096zm24.667-23.325-8.006 4.609 8.006 4.609 8.005-4.61zm-.801 10.605-4.646-2.675-3.363-1.936v9.124l4.645 2.674 3.364 1.937zm-18.422 20.561 11.743-6.704 5.87-3.35-8-4.606-9.211 5.303-8.395 4.833z" fill="#ff2d20"/>
+                                    </svg>
+                                </div>
+
+                                <p class="m-0 d-inline my-auto">
+                                    PHP Laravel
+                                </p>
+                            </div>
+
+                            <a href="#" class="d-block">
+                                <div>
+                                    <p class="fs-6 rounded-5 d-inline position-absolute px-3 py-1 btnMoreLa">
+                                        Voir plus
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- Fin projet Lravel -->
+                </div>
+            </div>
+            <!-- Fin projets -->
 
             <!-- Presentation veille informatique -->
             <div class="col-7 mx-auto mb-15 row bg-veille p-4 rounded-5 position-relative" id="veille">
@@ -565,6 +711,9 @@
                 });
             </script>
             <!-- Fin boutton retour haut de page -->
+
+
+            
         </main>
     </body>
 </html>
